fdbba86b2f2aaa43350fcb35461feb45f9bae743
tyfbyfby <fengboyuannj@gmail.com>
2017-02-17 15:59:56 -0500

    create project

---
+++ TestSimpleHello/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleHello;
+
+namespace TestSimpleHello
+{
+    [TestClass]
+    public class HelloOutputTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World", Program.CreateMessage());
+        }
+    }
+}
